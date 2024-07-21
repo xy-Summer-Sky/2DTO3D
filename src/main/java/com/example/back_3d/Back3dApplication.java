c1c537fc12be93d7fc20f065a48d334258899021
xy-Summer-Sky <summer-xy2022@outlook.com>
2024-07-21 18:16:39 +0800

    1

---
+++ src/main/java/com/example/back_3d/Back3dApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back3dApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Back3dApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/back_3d/Back3dApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back3dApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
